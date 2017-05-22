c230c520e91ac06d983d810f0f773a8ce371036c
Izaya <cancerizee@hotmail.fr>
2017-05-22 11:05:19 +0200

    Aide V2

--- miniProjet2017/miniProjet2017/Classes/Aide.cs
+++ miniProjet2017/miniProjet2017/Classes/Aide.cs
@@ -14,6 +14,7 @@ namespace miniProjet2017.Classes
         {
             Label lblAide = new System.Windows.Forms.Label();
             lblAide.Text = "Voici l'aide pour le formulaire de modification de transaction.";
+
             form.Controls.Add(lblAide);
         }
         public static void AideAjoutTransac()
