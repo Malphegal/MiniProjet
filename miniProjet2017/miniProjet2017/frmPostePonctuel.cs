f091b5eb2524b8a131531ec2a5562a2e2a5489ba
Izaya <cancerizee@hotmail.fr>
2017-06-07 13:47:02 +0200

    PostePonctuel V2

--- miniProjet2017/miniProjet2017/frmPostePonctuel.cs
+++ miniProjet2017/miniProjet2017/frmPostePonctuel.cs
@@ -44,7 +44,7 @@ namespace miniProjet2017
         {
             int topPnl = 170;
             int LeftPnl = 50;
-            int topElem = 50;
+            int topElem = 30;
             int leftElem = 50;
 
           //Version Panel
@@ -54,7 +54,9 @@ namespace miniProjet2017
 
             pnl.Text = "Liste des prélévements";
             pnl.Top = topPnl;
-            pnl.Left = LeftPnl;
+            pnl.Left = 30;
+            pnl.Width = 480;
+            pnl.Height = 300;
             pnl.AutoScroll = true;
             Controls.Add(pnl);
             Label lblTitre = new Label();
@@ -67,16 +69,20 @@ namespace miniProjet2017
             grb.Text = "Liste des prélévements";
             grb.Top = topPnl;
             grb.Left = LeftPnl;
-
             grb.Controls.Add(new VScrollBar());
             Controls.Add(grb); */
-            for (int j = 1; j < i; j++)
+            for (int j = 1; j <= i; j++)
             {
                 Label lbl = new Label();
+                DateTimePicker dtPick = new DateTimePicker();
+                TextBox txt2 = new TextBox();
+                dtPick.Top = topElem;
+                dtPick.Left = leftElem + lbl.Width;
                 lbl.Top = topElem;
                 lbl.Left = leftElem;
                 lbl.Text = "Prélévement n°" + j;
                 pnl.Controls.Add(lbl);
+                pnl.Controls.Add(dtPick);
               //grb.Controls.Add(lbl);
 
               //On incrémente les valeurs
