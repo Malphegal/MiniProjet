514004743ece40d993becf40ab82840507206250
Izaya <cancerizee@hotmail.fr>
2017-06-09 10:23:14 +0200

    Design Supprimer V1

---
+++ miniProjet2017/miniProjet2017/frmSupprPoste.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace miniProjet2017
+{
+    partial class frmSupprPoste
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBordure = new System.Windows.Forms.PictureBox();
+            this.picQuitter = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBordure)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picQuitter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBordure
+            // 
+            this.picBordure.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(49)))), ((int)(((byte)(60)))));
+            this.picBordure.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picBordure.Image = global::miniProjet2017.Properties.Resources.bordureOption;
+            this.picBordure.Location = new System.Drawing.Point(0, 0);
+            this.picBordure.Name = "picBordure";
+            this.picBordure.Size = new System.Drawing.Size(365, 334);
+            this.picBordure.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBordure.TabIndex = 12;
+            this.picBordure.TabStop = false;
+            // 
+            // picQuitter
+            // 
+            this.picQuitter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(49)))), ((int)(((byte)(60)))));
+            this.picQuitter.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picQuitter.Image = global::miniProjet2017.Properties.Resources.flecheRetour;
+            this.picQuitter.Location = new System.Drawing.Point(275, 21);
+            this.picQuitter.Name = "picQuitter";
+            this.picQuitter.Size = new System.Drawing.Size(69, 68);
+            this.picQuitter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picQuitter.TabIndex = 13;
+            this.picQuitter.TabStop = false;
+            this.picQuitter.Click += new System.EventHandler(this.QuitterFrmBudgetRecap);
+            // 
+            // frmSupprPoste
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 334);
+            this.Controls.Add(this.picQuitter);
+            this.Controls.Add(this.picBordure);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmSupprPoste";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Suppression de poste";
+            ((System.ComponentModel.ISupportInitialize)(this.picBordure)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picQuitter)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBordure;
+        private System.Windows.Forms.PictureBox picQuitter;
+    }
+}
---
+++ miniProjet2017/miniProjet2017/frmSupprPoste.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace miniProjet2017
+{
+    public partial class frmSupprPoste : Form
+    {
+        public frmSupprPoste()
+        {
+            InitializeComponent();
+        }
+
+        private void QuitterFrmBudgetRecap(object sender, EventArgs e)
+        {   QuitterFrmBudgetRecap
+            /* Ferme le frmBudgetRecap */
+        private void (object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+    }
+}
