60e81bf1fe1d2e08403fbc74e5d30e90e7f7a8d8
Izaya <cancerizee@hotmail.fr>
2017-06-09 14:09:54 +0200

    Bro :+1:

--- miniProjet2017/miniProjet2017/frmSupprPoste.cs
+++ miniProjet2017/miniProjet2017/frmSupprPoste.cs
@@ -32,6 +32,7 @@ namespace miniProjet2017
         /* Ferme le frmBudgetRecap */
         private void frmSupprPoste_Load(object sender, EventArgs e)
         {
+            flecheRetour = picQuitter.Image;
             new OleDbDataAdapter(new CMD(@"SELECT pp.*, p.libPoste FROM PostePeriodique pp, Poste p
                     WHERE pp.codePoste = p.codePoste", frmMain.con)).Fill(ds, "_PostePeriodique");
             new OleDbDataAdapter(new CMD(@"SELECT pp.*, p.libPoste FROM PostePonctuel pp, Poste p
@@ -69,54 +70,88 @@ namespace miniProjet2017
         {
                 // Sera faux s'il y a une erreur
 
-            bool toutEstOK = true;
+            bool toutEstOk = true;
 
                 // Test au cas par cas
+             if (cboListeType.SelectedIndex == -1)
+            {
+                errorProvider.SetError(cboListeType, "Il faut séléctionner un type pour cette transaction !");
+                toutEstOk = false;
+            }
+            else errorProvider.SetError(cboListeType, "");
 
-            MessageBox.Show("TOUTDUTEST");;
+            if (cboListeBudget.SelectedIndex == -1)
+            {
+                errorProvider.SetError(cboListeBudget, "Il faut séléctionner un budget !");
+                toutEstOk = false;
+            }
+            else errorProvider.SetError(cboListeBudget, "");
+            
 
+            if (toutEstOk)
+            {
                 // Suppr des poste
 
-            if (DialogResult.OK == MessageBox.Show("Test", "Test", MessageBoxButtons.OKCancel))
-            {
-                    // Suppression des postes au cas par cas
-
-                frmMain.con.Open();
-
-                int id = -1;
-
-                if (cboListeType.SelectedIndex == 0)
+                if (DialogResult.OK == MessageBox.Show("Test", "Test", MessageBoxButtons.OKCancel))
                 {
-                    id = Convert.ToInt32(ds.Tables["_PostePeriodique"].Rows[cboListeBudget.SelectedIndex][0]);
-                    new CMD(@"DELETE FROM PostePeriodique WHERE codePoste = "
-                        + ds.Tables["_PostePeriodique"].Rows[cboListeBudget.SelectedIndex][0], frmMain.con).ExecuteScalar();
-                }
-                else if (cboListeType.SelectedIndex == 1)
-                {
-                    id = Convert.ToInt32(ds.Tables["_PostePonctuel"].Rows[cboListeBudget.SelectedIndex][0]);
-                    new CMD(@"DELETE FROM Echeances WHERE codePoste = "
-                        + ds.Tables["_PostePonctuel"].Rows[cboListeBudget.SelectedIndex][0], frmMain.con).ExecuteScalar();
-                    new CMD(@"DELETE FROM PostePonctuel WHERE codePoste = "
-                        + ds.Tables["_PostePonctuel"].Rows[cboListeBudget.SelectedIndex][0], frmMain.con).ExecuteScalar();
-                }
-                else if (cboListeType.SelectedIndex == 2)
-                {
-                    id = Convert.ToInt32(ds.Tables["_PosteRevenu"].Rows[cboListeBudget.SelectedIndex][0]);
-                    new CMD(@"DELETE FROM PosteRevenu WHERE codePoste = "
-                        + ds.Tables["_PosteRevenu"].Rows[cboListeBudget.SelectedIndex][0], frmMain.con).ExecuteScalar();
-                }
-                    // Puis supprimer dans le Poste
-                    
-                new CMD(@"DELETE FROM Poste WHERE codePoste = " + id, frmMain.con).ExecuteNonQuery();
+                        // Suppression des postes au cas par cas
 
-                frmMain.con.Close();
+                    frmMain.con.Open();
 
-                MessageBox.Show("Le poste a été supprimé !");
+                    int id = -1;
 
-                frmSupprPoste_Load(sender, e);
+                    if (cboListeType.SelectedIndex == 0)
+                    {
+                        id = Convert.ToInt32(ds.Tables["_PostePeriodique"].Rows[cboListeBudget.SelectedIndex][0]);
+                        new CMD(@"DELETE FROM PostePeriodique WHERE codePoste = "
+                            + ds.Tables["_PostePeriodique"].Rows[cboListeBudget.SelectedIndex][0], frmMain.con).ExecuteScalar();
+                    }
+                    else if (cboListeType.SelectedIndex == 1)
+                    {
+                        id = Convert.ToInt32(ds.Tables["_PostePonctuel"].Rows[cboListeBudget.SelectedIndex][0]);
+                        new CMD(@"DELETE FROM Echeances WHERE codePoste = "
+                            + ds.Tables["_PostePonctuel"].Rows[cboListeBudget.SelectedIndex][0], frmMain.con).ExecuteScalar();
+                        new CMD(@"DELETE FROM PostePonctuel WHERE codePoste = "
+                            + ds.Tables["_PostePonctuel"].Rows[cboListeBudget.SelectedIndex][0], frmMain.con).ExecuteScalar();
+                    }
+                    else if (cboListeType.SelectedIndex == 2)
+                    {
+                        id = Convert.ToInt32(ds.Tables["_PosteRevenu"].Rows[cboListeBudget.SelectedIndex][0]);
+                        new CMD(@"DELETE FROM PosteRevenu WHERE codePoste = "
+                            + ds.Tables["_PosteRevenu"].Rows[cboListeBudget.SelectedIndex][0], frmMain.con).ExecuteScalar();
+                    }
+                        // Puis supprimer dans le Poste
+
+                    new CMD(@"DELETE FROM Poste WHERE codePoste = " + id, frmMain.con).ExecuteNonQuery();
+
+                    frmMain.con.Close();
+
+                    MessageBox.Show("Le poste a été supprimé !");
+
+                    frmSupprPoste_Load(sender, e);
+                }
             }
             else
                 MessageBox.Show("Aucune modification n'a été effectuée !");
+        }
+
+        private void picBordure_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        static Image flecheRetour_clair = new Bitmap("..\\..\\Resources\\flecheRetour_clair.png");
+        static Image flecheRetour;
+        /* Souris sur picQuitter */
+        private void SourisSurPicQuitter(object sender, EventArgs e)
+        {
+            (sender as PictureBox).Image = flecheRetour_clair;
+        }
+
+        /* Souris sors de picQuitter */
+        private void SourisSortDePicQuitter(object sender, EventArgs e)
+        {
+            (sender as PictureBox).Image = flecheRetour;
         }
     }
 }
